769ed048556d9677bacdb7fafb8e13ccc19a9852
VernellGowa <vnell2005@gmail.com>
2023-11-19 16:04:35 +0000

    fixed visual

--- src/main.py
+++ src/main.py
@@ -2,215 +2,365 @@ import tkinter as tk
 from tkinter import ttk
 import mysql.connector
 from PIL import Image, ImageTk
+from dataclasses import dataclass
+from tkcalendar import Calendar
+from datetime import datetime
+
+HEIGHT = 600
+WIDTH = 600
+
+class Database():
+    # def __init__(self):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Nelldestroyer25",
+        database="salon"
+    )
+    cursor = conn.cursor()
 
 class SalonApp(tk.Tk):
-	
-	# __init__ function for class SalonApp 
-	def __init__(self, *args, **kwargs): 
-		
-		# __init__ function for class Tk
-		tk.Tk.__init__(self, *args, **kwargs)
-		self.geometry("500x500")
-		
-		# creating a container
-		container = tk.Frame(self) 
-		container.pack(side = "top", fill = "both", expand = True) 
+    
+    # __init__ function for class SalonApp 
+    def __init__(self, *args, **kwargs): 
+        
+        # __init__ function for class Tk
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.geometry(F"{HEIGHT}x{HEIGHT}")
+        
+        # creating a container
+        container = tk.Frame(self) 
+        container.pack(side = "top", fill = "both", expand = True) 
 
-		container.grid_rowconfigure(0, weight = 1)
-		container.grid_columnconfigure(0, weight = 1)
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
 
-		# initializing frames to an empty array
-		self.frames = {} 
+        # initializing frames to an empty array
+        self.frames = {} 
 
-		# iterating through a tuple consisting
-		# of the different page layouts
-		for F in (LoginPage, RegisterPage, HomePage):
+        # iterating through a tuple consisting
+        # of the different page layouts
+        for F in (LoginPage, RegisterPage, HomePage, ServicePage, DatePicker):
 
-			frame = F(container, self)
+            frame = F(container, self)
 
-			# initializing frame of that object from
-			# startpage, page1, page2 respectively with 
-			# for loop
-			self.frames[F] = frame 
+            # initializing frame of that object from
+            # startpage, page1, page2 respectively with 
+            # for loop
+            self.frames[F] = frame 
 
-			frame.grid(row = 0, column = 0, sticky ="nsew")
+            frame.grid(row = 0, column = 0, sticky ="nsew")
 
-		self.show_frame(LoginPage)
+        self.show_frame(LoginPage)
 
-	# to display the current frame passed as
-	# parameter
-	def show_frame(self, cont):
-		frame = self.frames[cont]
-		frame.tkraise()
-		
+    # to display the current frame passed as
+    # parameter
+    # def show_frame(self, cont):
+    #     frame = self.frames[cont]
+    #     frame.tkraise()
+
+    def show_frame(self, cont, *arg):
+        frame = self.frames[cont]
+        if arg is not None and hasattr(frame, 'set_data'):
+            frame.set_data(arg)
+        frame.tkraise()
+
 class LoginPage(tk.Frame):
-	def __init__(self, parent, controller):
-		tk.Frame.__init__(self, parent)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-		self.controller = controller
+        self.controller = controller
 
-		# Email Address
-		label_email = tk.Label(self, text="Email Address:")
-		label_email.pack()
-		entry_email = tk.Entry(self)
-		entry_email.pack(pady=5)
+        # Email Address
+        label_email = tk.Label(self, text="Email Address:")
+        label_email.pack()
+        entry_email = tk.Entry(self)
+        entry_email.pack(pady=5)
 
-		# Password
-		label_password = tk.Label(self, text="Password:")
-		label_password.pack()
-		entry_password = tk.Entry(self, show="*")
-		entry_password.pack(pady=5)
+        # Password
+        label_password = tk.Label(self, text="Password:")
+        label_password.pack()
+        entry_password = tk.Entry(self, show="*")
+        entry_password.pack(pady=5)
 
-		# Login Button
-		login_button = tk.Button(self, text="Login", command=lambda: self.login(entry_email.get(), entry_password.get()))
-		login_button.pack(pady=10)
+        # Login Button
+        login_button = tk.Button(self, text="Login", command=lambda: self.login(entry_email.get(), entry_password.get()))
+        login_button.pack(pady=10)
 
-		self.login_status_label = tk.Label(self, text="")
-		self.login_status_label.pack()
+        self.login_status_label = tk.Label(self, text="")
+        self.login_status_label.pack()
 
-		# Navigate to Sign Up Page Button
-		sign_up_button = tk.Button(self, text="Go to Sign Up", command=lambda: controller.show_frame(RegisterPage))
-		sign_up_button.pack(pady=5)
+        # Navigate to Sign Up Page Button
+        sign_up_button = tk.Button(self, text="Go to Sign Up", command=lambda: controller.show_frame(RegisterPage))
+        sign_up_button.pack(pady=5)
 
-	def login(self, email, password):
-		# Check if the email and password match a customer in the database
-		query = "SELECT customer_id FROM customers WHERE email = %s AND password = %s"
-		cursor.execute(query, (email, password))
-		result = cursor.fetchone()
+    def login(self, email, password):
+        # Check if the email and password match a customer in the database
+        # query = "SELECT customer_id FROM customers WHERE email = %s AND password = %s"
+        # Database.cursor.execute(query, (email, password))
+        # result = Database.cursor.fetchone()
 
-		if result:
-			customer_id = result[0]
-			self.controller.show_frame(HomePage(customer_id))
-		else:
-			self.login_status_label.config(text="Invalid email or password", fg="red")
+        # if result:
+        #     customer_id = result[0]
+        #     self.controller.show_frame(HomePage, customer_id)
+        # else:
+        #     self.login_status_label.config(text="Invalid email or password", fg="red")
+        self.controller.show_frame(HomePage, 9)
 
-class RegisterPage(tk.Frame):
-	def __init__(self, parent, controller):
-		tk.Frame.__init__(self, parent)
 
-		self.controller = controller
+class RegisterPage(tk.Frame, Database):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-		# First Name
-		label_first_name = tk.Label(self, text="First Name:")
-		label_first_name.pack()
-		entry_first_name = tk.Entry(self)
-		entry_first_name.pack(pady=5)
+        self.parent = parent
+        self.controller = controller
 
-		# Last Name
-		label_last_name = tk.Label(self, text="Last Name:")
-		label_last_name.pack()
-		entry_last_name = tk.Entry(self)
-		entry_last_name.pack(pady=5)
+        # First Name
+        label_first_name = tk.Label(self, text="First Name:")
+        label_first_name.pack()
+        entry_first_name = tk.Entry(self)
+        entry_first_name.pack(pady=5)
 
-		# Phone Number
-		label_phone_number = tk.Label(self, text="Phone Number:")
-		label_phone_number.pack()
-		entry_phone_number = tk.Entry(self)
-		entry_phone_number.pack(pady=5)
+        # Last Name
+        label_last_name = tk.Label(self, text="Last Name:")
+        label_last_name.pack()
+        entry_last_name = tk.Entry(self)
+        entry_last_name.pack(pady=5)
 
-		# Gender
-		label_gender = tk.Label(self, text="Gender:")
-		label_gender.pack()
-		gender_var = tk.StringVar()
+        # Phone Number
+        label_phone_number = tk.Label(self, text="Phone Number:")
+        label_phone_number.pack()
+        entry_phone_number = tk.Entry(self)
+        entry_phone_number.pack(pady=5)
 
-		gender_frame = tk.Frame(self)
-		gender_frame.pack(pady=5)
+        # Gender
+        label_gender = tk.Label(self, text="Gender:")
+        label_gender.pack()
+        gender_var = tk.StringVar(value="")
 
-		gender_button_male = tk.Radiobutton(gender_frame, text="Male", variable=gender_var, value="Male")
-		gender_button_male.pack(side=tk.LEFT)
+        gender_frame = tk.Frame(self)
+        gender_frame.pack(pady=5)
 
-		gender_button_female = tk.Radiobutton(gender_frame, text="Female", variable=gender_var, value="Female")
-		gender_button_female.pack(side=tk.LEFT)
+        gender_button_male = tk.Radiobutton(gender_frame, text="Male", variable=gender_var, value="Male")
+        gender_button_male.pack(side=tk.LEFT)
 
-		gender_button_other = tk.Radiobutton(gender_frame, text="Other", variable=gender_var, value="Other")
-		gender_button_other.pack(side=tk.LEFT)
+        gender_button_female = tk.Radiobutton(gender_frame, text="Female", variable=gender_var, value="Female")
+        gender_button_female.pack(side=tk.LEFT)
 
-		# Email Address
-		label_email = tk.Label(self, text="Email Address:")
-		label_email.pack()
-		entry_email = tk.Entry(self)
-		entry_email.pack(pady=5)
+        gender_button_other = tk.Radiobutton(gender_frame, text="Other", variable=gender_var, value="Other")
+        gender_button_other.pack(side=tk.LEFT)
 
-		# Password
-		label_password = tk.Label(self, text="Password:")
-		label_password.pack()
-		entry_password = tk.Entry(self, show="*")
-		entry_password.pack(pady=5)
+        # Email Address
+        label_email = tk.Label(self, text="Email Address:")
+        label_email.pack()
+        entry_email = tk.Entry(self)
+        entry_email.pack(pady=5)
 
-		# Submit Button
-		submit_button = tk.Button(self, text="Submit", command=lambda: self.submit(entry_first_name.get(), entry_last_name.get(), entry_phone_number.get(),
-																	gender_var.get(), entry_email.get(), entry_password.get()))
-		submit_button.pack(pady=10)
+        # Password
+        label_password = tk.Label(self, text="Password:")
+        label_password.pack()
+        entry_password = tk.Entry(self, show="*")
+        entry_password.pack(pady=5)
 
-		# Navigate to Login Page Button
-		login_button = tk.Button(self, text="Go to Login", command=lambda: controller.show_frame(LoginPage))
-		login_button.pack(pady=5)
-		
-	def submit(self, first_name,last_name,phone_number,gender,email,password):
-		# Check if the email and password match a customer in the database
-		query = "SELECT customer_id FROM customers WHERE email = %s AND password = %s"
-		cursor.execute(query, (email, password))
-		result = cursor.fetchone()
+        # Submit Button
+        submit_button = tk.Button(self, text="Submit", command=lambda: self.submit(entry_first_name.get(), entry_last_name.get(), entry_phone_number.get(),
+                                                                    gender_var.get(), entry_email.get(), entry_password.get()))
+        submit_button.pack(pady=10)
 
-		if result:
-			customer_id = result[0]
-			self.controller.show_frame(HomePage(customer_id))
-		else:
-			self.login_status_label.config(text="Invalid email or password", fg="red")
+        # Navigate to Login Page Button
+        login_button = tk.Button(self, text="Go to Login", command=lambda: controller.show_frame(LoginPage))
+        login_button.pack(pady=5)
 
+        self.register_status_label = tk.Label(self, text="")
+        self.register_status_label.pack()
+        
+    def submit(self, first_name,last_name,phone_number,gender,email,password):
+        # Check if the email and password match a customer in the database
+        query = "SELECT customer_id FROM customers WHERE email = %s"
+        Database.cursor.execute(query, (email,))
+        result = Database.cursor.fetchone()
+
+        if result:
+            self.register_status_label.config(text="Email already registered!", fg="red")
+        else:
+            # Insert a new customer into the database
+            query = "INSERT INTO customers (first_name, last_name, phone_number, gender, email, password) VALUES (%s, %s, %s, %s, %s, %s)"
+            Database.cursor.execute(query, (first_name, last_name, phone_number, gender, email, password))
+            Database.conn.commit()
+            customer_id = Database.cursor.lastrowid
+            self.controller.show_frame(HomePage, customer_id)
 
 class HomePage(tk.Frame): 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Home")
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+        self.controller = controller
+        self.customer_id = None
 
-        styles = [
-            {'image': 'image.png', 'name': 'Style 1', 'description': 'Description 1', 'price': 'Price 1', 'duration': 'Duration 1'},
-            {'image': 'image.png', 'name': 'Style 2', 'description': 'Description 2', 'price': 'Price 2', 'duration': 'Duration 2'},
-            # Add more styles as needed
-        ]
+        # Create a canvas and a vertical scrollbar
+        self.canvas = tk.Canvas(self, bg='light blue')
+        style = ttk.Style()
+        style.configure("Blue.TFrame", background="light blue")
 
-        for i, style in enumerate(styles):
-            card = tk.Frame(self)
-            img = Image.open(style['image'])
+        self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = ttk.Frame(self.canvas, style="Blue.TFrame")
+
+        # Configure the canvas to use the scrollbar
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        # Pack the scrollbar and the canvas
+        self.scrollbar.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+
+        # Add the scrollable frame to the canvas at the center position
+        self.canvas.create_window((WIDTH//2, HEIGHT), window=self.scrollable_frame, anchor="center")
+
+        # Configure the scroll region of the canvas whenever its size changes
+        self.scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+
+        label = ttk.Label(self.scrollable_frame, text ="Home", background="light blue", font=("Verdana", 35))
+        label.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+        query = "SELECT service_id, name, description, price, duration, image FROM services"
+        Database.cursor.execute(query, ())
+        results = Database.cursor.fetchall()
+
+        for i, result in enumerate(results):
+            card = tk.Frame(self.scrollable_frame, relief=tk.RAISED, borderwidth=3)
+
+            img = Image.open(result[5])
             img = ImageTk.PhotoImage(img)
             img_label = tk.Label(card, image=img)
             img_label.image = img
-            img_label.grid(row=0, column=0, columnspan=4)
+            img_label.grid(row=0, column=0, padx=30, pady=10)
 
-            name_label = ttk.Label(card, text=style['name'])
+            name_label = ttk.Label(card, text=result[1])
             name_label.grid(row=1, column=0)
 
-            desc_label = ttk.Label(card, text=style['description'])
-            desc_label.grid(row=1, column=1)
+            desc_label = ttk.Label(card, text=result[2], wraplength=300, background=card.cget('bg'))
+            desc_label.grid(row=2, column=0)
 
-            price_label = ttk.Label(card, text=style['price'])
-            price_label.grid(row=1, column=2)
+            price_label = ttk.Label(card, text=result[3])
+            price_label.grid(row=3, column=0)
 
-            duration_label = ttk.Label(card, text=style['duration'])
-            duration_label.grid(row=1, column=3)
+            duration_label = ttk.Label(card, text=result[4])
+            duration_label.grid(row=4, column=0, pady=10)
 
-            card.grid(row=i+1, column=0, padx=10, pady=10)
+            for child in card.winfo_children():
+                child.bind("<Button-1>", lambda e, service=result: self.show_service_page(service))
 
-# class HomePage(tk.Frame): 
-# 	def __init__(self, parent, controller):
-# 		tk.Frame.__init__(self, parent)
-# 		label = ttk.Label(self, text ="Home")
-			
-# 		label.grid(row = 0, column = 4, padx = 10, pady = 10)
+            card.bind("<Button-1>", lambda e, service=result: self.show_service_page(service))
+            card.grid(row=i+1, column=0, pady=10)
+
+    def set_data(self, customer_id):
+        self.customer_id = customer_id[0]
+
+
+    def show_service_page(self, service):
+        # Show the ServicePage with the details of the selected service
+        self.controller.show_frame(ServicePage, service, self.customer_id)
+
+class ServicePage(tk.Frame): 
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.service = None
+
+        self.back_button = ttk.Button(self, text="Back", command=self.handle_back)
+        self.back_button.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
+
+        self.image_label = tk.Label(self)
+        self.image_label.grid(row=1, column=0)
+
+        self.name_label = ttk.Label(self)
+        self.name_label.grid(row=2, column=0)
+
+        self.desc_label = ttk.Label(self)
+        self.desc_label.grid(row=3, column=0)
+
+        self.price_label = ttk.Label(self)
+        self.price_label.grid(row=4, column=0)
+
+        self.duration_label = ttk.Label(self)
+        self.duration_label.grid(row=5, column=0)
+
+        self.book_button = ttk.Button(self, text="Book Appointment", command=self.book_appointment)
+        self.book_button.grid(row=6, column=0)
+
+
+
+    def handle_back(self):
+        self.controller.show_frame(HomePage, self.customer_id)
+
+    def set_data(self, args):
+        self.service = args[0]
+        self.customer_id = args[1]
+
+        # Load the image
+        img = Image.open(self.service[5])
+        img = ImageTk.PhotoImage(img)
+        self.image_label.config(image=img)
+        self.image_label.image = img
+
+        # Update the labels with the service details
+        self.name_label.config(text=self.service[1])
+        self.desc_label.config(text=self.service[2])
+        self.price_label.config(text=self.service[3])
+        self.duration_label.config(text=self.service[4])
+
+    def book_appointment(self):
+        # Code to book an appointment
+        self.controller.show_frame(DatePicker, self.service, self.customer_id)
+        # query = "INSERT INTO bookings (customer_id, service_id) VALUES (%s, %s)"
+        # Database.cursor.execute(query, (self.customer_id, self.service[0]))
+        # Database.connection.commit()
+
+
+class DatePicker(tk.Frame): 
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.service = None
+
+        currentSecond= datetime.now().second
+        currentMinute = datetime.now().minute
+        currentHour = datetime.now().hour
+
+        currentDay = datetime.now().day
+        currentMonth = datetime.now().month
+        currentYear = datetime.now().year
+
+        self.back_button = ttk.Button(self, text="Back", command=self.handle_back).pack(pady = 20)
+
+        # Add Calendar
+        self.cal = Calendar(self, selectmode = 'day',
+                    year = currentYear, month = currentMonth,
+                    day = currentDay,)
+
+
+        self.cal.pack(pady = 20)
+        
+        # Add Button and Label
+        tk.Button(self, text = "Get Date",
+            command = self.send_date()).pack(pady = 20)
+
+
+    def handle_back(self):
+        self.controller.show_frame(ServicePage, self.service, self.customer_id)
+
+    def set_data(self, args):
+        self.service = args[0]
+        self.customer_id = args[1]
+
+    def send_date(self):
+        print(self.cal.get_date())
+
 
 app = SalonApp()
 app.mainloop()
 
 if __name__ == "__main__":
-	db = mysql.connector.connect(
-		host="localhost",
-		user="self",
-		password="Nelldestroyer25",
-		database="salon"
-	)
+    app = SalonApp()
+    app.mainloop()
 
-	with db.cursor() as cursor:
-		app = SalonApp()
-		app.mainloop()
+# test@gmail.com
+# pass
--- src/x.py
+++ src/x.py
@@ -1,161 +1,39 @@
-import tkinter as tk
-import tkinter.messagebox as messagebox
-import mysql.connector
-import hashlib  # For password hashing
+# Import Required Library
+from tkinter import *
+from tkcalendar import Calendar
+from datetime import datetime
 
-class SalonApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Salon - Login")
-        self.root.geometry("500x500")
+currentSecond= datetime.now().second
+currentMinute = datetime.now().minute
+currentHour = datetime.now().hour
 
-        self.create_login_screen()
+currentDay = datetime.now().day
+currentMonth = datetime.now().month
+currentYear = datetime.now().year
 
-    def create_login_screen(self):
-        # Create a frame to group login elements
-        login_frame = tk.Frame(self.root)
-        login_frame.pack()
-
-        # Email entry
-        email_label = tk.Label(login_frame, text="Email:")
-        email_label.pack()
-        self.email_entry = tk.Entry(login_frame)
-        self.email_entry.pack()
-
-        # Password entry
-        password_label = tk.Label(login_frame, text="Password:")
-        password_label.pack()
-        self.password_entry = tk.Entry(login_frame, show="*")
-        self.password_entry.pack()
-
-        # Login button
-        login_button = tk.Button(login_frame, text="Login", command=self.login)
-        login_button.pack()
-
-        # Sign-up button
-        sign_up_button = tk.Button(login_frame, text="Sign Up", command=self.create_signup_screen)
-        sign_up_button.pack()
-
-        # Login status label
-        self.login_status_label = tk.Label(login_frame, text="")
-        self.login_status_label.pack()
-
-    def login(self):
-        email = self.email_entry.get()
-        password = self.password_entry.get()
-
-        # Check if the email and hashed password match a customer in the database
-        query = "SELECT customer_id, password FROM customers WHERE email = %s"
-        cursor.execute(query, (email,))
-        result = cursor.fetchone()
-
-        if result:
-            customer_id, stored_password = result
-            if hashlib.sha256(password.encode()).hexdigest() == stored_password:
-                self.show_home_screen(customer_id)
-            else:
-                self.login_status_label.config(text="Invalid email or password", fg="red")
-        else:
-            self.login_status_label.config(text="Invalid email or password", fg="red")
+# Create Object
+root = Tk()
+ 
+# Set geometry
+root.geometry("400x400")
+ 
+# Add Calendar
+cal = Calendar(root, selectmode = 'day',
+               year = currentYear, month = currentMonth,
+               day = currentDay,)
 
 
-    def create_signup_screen(self):
-        # Create a frame to group sign-up elements
-        self.root.destroy()
-        signup_frame = tk.Frame(self.root)
-        signup_frame.pack()
-
-        # Full Name entry
-        full_name_label = tk.Label(signup_frame, text="Full Name:")
-        full_name_label.pack()
-        self.full_name_entry = tk.Entry(signup_frame)
-        self.full_name_entry.pack()
-
-        # Email entry
-        email_label = tk.Label(signup_frame, text="Email:")
-        email_label.pack()
-        self.email_entry = tk.Entry(signup_frame)
-        self.email_entry.pack()
-
-        # Password entry
-        password_label = tk.Label(signup_frame, text="Password:")
-        password_label.pack()
-        self.password_entry = tk.Entry(signup_frame, show="*")
-        self.password_entry.pack()
-
-        # Sign-up button
-        signup_button = tk.Button(signup_frame, text="Sign Up", command=self.register_user)
-        signup_button.pack()
-
-        # Sign-up status label
-        self.signup_status_label = tk.Label(signup_frame, text="")
-        self.signup_status_label.pack()
-
-    def register_user(self):
-        full_name = self.full_name_entry.get()
-        email = self.email_entry.get()
-        password = self.password_entry.get()
-
-        if not (full_name and email and password):
-            self.signup_status_label.config(text="Please fill in all fields", fg="red")
-            return
-
-        # Check if the email is already registered
-        query = "SELECT customer_id FROM customers WHERE email = %s"
-        cursor.execute(query, (email,))
-        result = cursor.fetchone()
-
-        if result:
-            self.signup_status_label.config(text="Email is already registered", fg="red")
-        else:
-            # Hash the password before storing it in the database
-            hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-            # Insert the new customer into the database
-            query = "INSERT INTO customers (full_name, email, password) VALUES (%s, %s, %s)"
-            cursor.execute(query, (full_name, email, hashed_password))
-            db.commit()  # Commit the transaction
-
-            self.signup_status_label.config(text="Registration successful!", fg="green")
-
-    def show_home_screen(self, customer_id):
-        # Destroy the login screen
-        # login_screen.destroy()
-
-        # Create the home screen
-        home_screen = tk.Tk()
-        home_screen.title("Salon - Home")
-        home_screen.geometry("500x500")
-
-        # Get all services from the database
-        query = "SELECT service_id, name, price FROM services"
-        cursor.execute(query)
-        services = cursor.fetchall()
-
-        # Display services
-        services_label = tk.Label(home_screen, text="Services")
-        services_label.pack()
-
-        for service in services:
-            service_label = tk.Label(
-                home_screen,
-                text=f"{service[1]} - ${service[2]:.2f}",
-                padx=10,
-                pady=5
-            )
-            service_label.pack()
-
-        home_screen.mainloop()
-
-if __name__ == "__main__":
-    db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Nelldestroyer25",
-        database="salon"
-    )
-
-    # Use a context manager for the cursor
-    with db.cursor() as cursor:
-        app = SalonApp(tk.Tk())
-        tk.mainloop()
+cal.pack(pady = 20)
+ 
+def grad_date():
+    date.config(text = "Selected Date is: " + cal.get_date())
+ 
+# Add Button and Label
+Button(root, text = "Get Date",
+       command = grad_date).pack(pady = 20)
+ 
+date = Label(root, text = "")
+date.pack(pady = 20)
+ 
+# Execute Tkinter
+root.mainloop()
